edecde1a16e273a504ebfbeed35a18f0d89e623b
mahdi <117587675+themahdiwm@users.noreply.github.com>
2022-11-13 20:39:01 +0330

    Add files via upload

---
+++ vowel.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+main() {
+	
+char English;
+cout<<"please Enter an English Word;";
+cin>>English;
+switch (English){
+	
+	case 'a':
+	cout<<"VOWEL";
+	break;
+	case'e':
+	cout<<"VOWEL";
+	break;
+	case'i':
+	cout<<"VOWEL";
+	break;
+	case'o':
+	cout<<"VOWEL";
+	break;
+	case'u':
+	cout<<"VOWEL";
+	break;	
+}
+}
