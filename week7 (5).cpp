8990a1bf2a2a0d4d6bec698f9ab86ac9d6e12dd9
mahdi <117587675+themahdiwm@users.noreply.github.com>
2022-11-26 21:15:43 +0330

    Add files via upload

---
+++ week7 (5).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+ main() {
+	int a,b,c,s;
+	cout<<"enter a positive number:";
+	cin>>b;
+	s=0;
+	for(a=2; c<=(a/2);c++)
+		if (a%c==0)
+
+	{c=a;
+	break;
+	}
+	if (c!=a)
+
+	{s+=a;
+	}
+	a+=2;
+	cout<<"result:"<<s;
+}
